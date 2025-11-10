5641a3498e3f5390170694eefe8c3ca350af3935
Marcel Bartholet <Marcel.Bartholet@web.de>
2025-11-10 08:46:23 +0100

    fix deployment url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://yawp.zordak-modding.net',
+  url: 'https://z0rdak.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/yawp-Docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
